033febd135acd13f8cbad45404a11fecebdce167
AngelusSilva15 <102191428+AngelusSilva15@users.noreply.github.com>
2022-05-06 19:53:14 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world, backgroundImg;
+
+var canvas, angle, tower, ground, cannon;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+}
+
+function setup() {
+
+  canvas = createCanvas(windowWidth,windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  
+angeleMode(DEGREES);
+
+  var options = {
+    isStatic: true
+  };
+
+  playerBase = Bodies.rectangle(200, 350, 150, options);
+  World.add(world, ground);
+
+  player = Bodies.rectangle(250,playerBase.position.y - 160,50,180, options);
+  World.add(world,player );
+
+}
+
+function draw() {
+  background(backgroundImg)
+
+  Engine.update(engine)
+image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+image(playerimage,player.position.x,player.position.y,50,180)
+
+drawSprite()
+}
